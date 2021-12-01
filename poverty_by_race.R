aadfa9dff4ad7a6dc1f0052e73ac5d634fb9ccf5
Kyle Dao <kyledao@Kyles-MacBook-Pro-3.local>
2021-11-30 16:41:42 -0800

    Created bar graph for poverty by race in King County

---
+++ poverty_by_race.R
@@ -0,0 +1,28 @@
+library("tidyverse")
+library("ggplot2")
+library("lintr")
+library("styler")
+library("mapproj")
+
+poverty_data <- read.csv("data/Poverty by Race and Ethnicity.csv")
+
+# Average poverty percentage across years
+poverty_by_race <- poverty_data %>%
+  group_by(Race) %>%
+  summarize(mean_percent = round(mean(share), 2))
+
+# Creating bar chart for data
+bar_plot <- ggplot(data=poverty_by_race, aes(x=Race, y=mean_percent)) +
+  geom_bar(stat="identity", fill="steelblue")+
+  geom_text(aes(label=mean_percent), vjust=-0.3, size=3.5) +
+  theme_minimal() + 
+  labs(
+    title = "Percentage of Race in Poverty in King County",
+    x = "Races & Ethnicities",
+    y = "Percentage of Race in Poverty"
+  )
+
+bar_plot
+
+lint("difference_in_wage_by_gender.R")
+style_file("difference_in_wage_by_gender.R")
